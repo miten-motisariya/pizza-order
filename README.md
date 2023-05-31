b0231cc1eb25b3b20bad670be562a1dd84fc5347
Miten M <miten.fameux@gmail.com>
2023-05-31 12:15:13 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Getting Started with Pizza Order Chatbot 🍕 App
+
+This is a Pizza Order Lex Chat Bot, a web application built using React and AWS Lex. It allows users to interact with a chatbot interface to place orders for pizzas.
+
+## Prerequisites
+To run this app locally, you need to have the following installed:
+
+- Node.js
+- AWS account with Lex service configured
+
+## Installation
+To install the necessary dependencies, follow these steps:
+
+1. Clone the repository: 
+```
+git clone https://github.com/your-username/pizza-order-lex-chat-bot.git
+```
+2. Navigate to the project directory: cd pizza-order-lex-chat-bot
+3. Install dependencies: npm install
+
+## Configuration
+Before running the app, make sure to configure the AWS credentials and AWS Lex bot details. Update the configuration files with your own AWS credentials and bot details:
+
+- ```.env```:  Update the REACT_APP_LEXBOT_IDENTITY_KEY, REACT_APP_BOT_ID, REACT_APP_BOT_ALIAS_ID, and REACT_APP_BOT_LOCALE_ID with your own AWS credentials and bot name.
+
+if you want to create Pizza Lex Chat Bot then clone the [Lexchatbot](https://github.com/miten-motisariya/Lexchatbot.git)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or would like to add new features, feel free to open
---
+++ src/App.js
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import { LexRuntimeV2 } from "aws-sdk";
+import sendBtn from "./send-message.png";
+import "./style.css";
+
+const AWS = require("aws-sdk");
+
+const LexChatbot = () => {
+  const [messages, setMessages] = useState([]);
+  const [text, setText] = useState("");
+  const [imageResponseCard, setImageResponseCard] = useState([]);
+  const [sessionId, setSessionId] = useState(`${Date.now()}`);
+  
+  const handleSubmit = async (event) => {
+    if (event.preventDefault) event?.preventDefault();
+    const temptext = event.isImageCard ? event.text : text;
+    const tempMessage = [
+      ...messages,
+      {
+        isSend: true,
+        content: temptext,
+      },
+    ];
+    setText("");
+    setMessages(tempMessage);
+    AWS.config.region = 'us-east-1';
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: process.env.REACT_APP_LEXBOT_IDENTITY_KEY // change the Identity Pool
+    });
+    const client = new LexRuntimeV2({
+      apiVersions: "2020-08-07",
+      correctClockSkew: true,
+      region: "us-east-1",
+    });
+
+    client.recognizeText(
+      {
+        botId: process.env.REACT_APP_BOT_ID,
+        botAliasId: process.env.REACT_APP_BOT_ALIAS_ID,
+        localeId: process.env.REACT_APP_BOT_LOCALE_ID,
+        text: temptext,
+        sessionId: sessionId,
+      },
+      (err, data) => {
+        if (err) {
+          console.error(err);
+          setMessages([...tempMessage, {
+            isSend: false,
+            isImageResponseCard: false,
+            content: "Hello, Sorry to say that, We are unable to procced your request.",
+          }])
+        } else {
+          const messages = data.messages ?? []
+          if (messages) {
+           const dataMessage = messages.map((element, index)=>{
+              return {
+                messages: element,
+                isSend: false,
+                isImageResponseCard: element.contentType === "ImageResponseCard",
+                content: element.content ?? element.imageResponseCard.title
+              }
+            });
+            setMessages([...tempMessage, ...dataMessage] ?? []);
+          }
+          if (data.messages[0].contentType === "ImageResponseCard") {
+            setImageResponseCard(data.messages[0].imageResponseCard);
+          }
+          setText("");
+        }
+        if (data.sessionState) {
+          if (data.sessionState?.dialogAction.type === "Close") {
+            client.deleteSession({
+              botId: process.env.REACT_APP_BOT_ID,
+              botAliasId: process.env.REACT_APP_BOT_ALIAS_ID,
+              localeId: process.env.REACT_APP_BOT_LOCALE_ID,
+              sessionId: sessionId,
+            });
+            setSessionId(`${Date.now()}`);
+          }
+        }
+      }
+    );
+  };
+
+  const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: "2-digit" });
+
+  return (
+    <div className="chatbot-container">
+      <div id="header">
+        <h1>Pizza Order Chatbot 🍕</h1>
+      </div>
+      <div id="chatbot">
+        <div id="conversation">
+          {messages.map((message, index) => (
+            <>
+              <div
+                className={`chatbot-message ${
+                  message.isSend ? "user-message" : ""
+                }`}
+              >
+                <p className={`chatbot-text`} senttime={currentTime}>{message?.content}</p>
+              </div>
+              <div
+                className={`chatbot-message ${
+                  message.isSend ? "user-message" : ""
+                }`}
+              >
+              </div>
+              {message.isImageResponseCard ? (
+                  <div className="chatbot-message-card">
+                    <div className="card">
+                    {imageResponseCard.imageUrl ? (<img className="imgRes" src={imageResponseCard.imageUrl} alt="imagesResponses" />):(<></>)}
+                      <div className="card__details">
+                        <div className="name">{imageResponseCard.title}</div>
+                        {imageResponseCard.subtitle ? (<p className="p-desc">{imageResponseCard.subtitle}</p>):(<></>)}
+                        {imageResponseCard.buttons.map((card) => {
+                          return (
+                            <button className="buttonRes" onClick={() => {
+                              handleSubmit({
+                                isImageCard: true,
+                                text: card.text,
+                              });
+                            }}>{card.text}</button>
+                          );
+                          })}
+                      </div>
+                    </div>
+                  </div>
+                ) : (<></>)}
+            </>
+          ))}
+        </div>
+        <form id="input-form">
+          <div className="message-container">
+            <input
+              id="input-field"
+              type="text"
+              placeholder="Type your message here"
+              value={text}
+              onChange={(event) => setText(event.target.value)}
+            />
+            <button id="submit-button" disabled={!text} type="submit" onClick={handleSubmit}>
+              <img className="send-icon" src={sendBtn} alt="" />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LexChatbot;
